96a50b90619d776c3ba43ae5492a8264fe4247dc
Raou <oucemar@gmail.com>
2025-06-21 15:21:39 -0300

    Added comments to scripts

--- Demo/NPCs/entity.gd
+++ Demo/NPCs/entity.gd
@@ -1,6 +1,9 @@
 class_name Entity extends Node
 
-
+### I created this Entity component just to handle functions related to the CharacterID
+### You can replace this with whatever other component makes more sense for your project
+### Maybe you leave all your character data inside a CharacterBody script, in that case
+### just connect the signal correctly and place the correct functions and references inside your script
 
 
 @export var characterID: CharacterID
@@ -10,6 +13,13 @@ func _ready():
 	Global.set_character_idle_animation.connect(_on_idle_animation)
 	
 func _on_idle_animation(_char: CharacterID,_anim: String):
+	### This funcion checks if the characterID assigned to the Dialogue Response is the same as this one.
+	### If it is, and you assigned an animation player to this node, and you also passed an animation name in the Response resource
+	### it'll play it.
 	if _char == characterID and animationPlayer != null:
 		if _anim:
 			animationPlayer.play(_anim)
+			
+	### You can expand this to do other things, maybe its a 3D project and you have a facial animation system?
+	### You could pass this information through the DialogueResponse resource and pass it through the signal
+	### and then use that information in your facial animation system.
--- Demo/dialogue_component_edited.gd
+++ Demo/dialogue_component_edited.gd
@@ -1,5 +1,12 @@
 extends DialogueComponent
 
+### This is an edited version of the Dialogue Component
+### I created it specifically for this demo to handle the interaction between player and NPCs
+### Your project will probably handle interactions differently
+### in that case, you can ignore this and just call the _start() function through there
+
+
+
 @export var range: float = 25
 func _process(delta):
 	var dis = get_parent().global_position.distance_to(Global.player.global_position)
